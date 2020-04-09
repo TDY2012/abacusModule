70d70846c4bc92a56f869e03132d186615383eb7
tdy2012 <tdy2020@gmail.com>
2020-04-09 14:48:40 +0700

    [Master] Implement function generate math expression with given constraint

--- Assets/Scripts/ExpressionManager.cs
+++ Assets/Scripts/ExpressionManager.cs
@@ -1,22 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExpressionManager : MonoBehaviour
 {
     public uint result;
-    public OperatorType operatorType;
-    public uint expectedDigitNum;
-    public uint biasDigitNum;
+    public uint numOperator;
+    public uint expectedNumDigit;
+    public OperatorType[] allowOperatorTypeArray;
 
     // Start is called before the first frame update
     void Start()
     {
-        uint[] r = MathExpressionGenerator.GetLowerExpectedRange(result, operatorType, expectedDigitNum, biasDigitNum);
-        string log = "";
-        foreach(uint x in r){
-            log += x + ",";
-        }
-        Debug.Log(log);
+        MathExpression e = MathExpressionGenerator.GenerateMathExpressionByValue( result, numOperator, expectedNumDigit, allowOperatorTypeArray );
+        Debug.Log(e.ToString());
+        /*
+        System.Random random = new System.Random();
+        uint numOperator = 2;
+        uint firstNumOperator = (uint)random.Next(0, (int)numOperator);
+        uint secondNumOperator = numOperator - firstNumOperator - 1;
+        Debug.Log(firstNumOperator);
+        Debug.Log(secondNumOperator);
+        */
     }
 }
--- Assets/Scripts/MathExpression.cs
+++ Assets/Scripts/MathExpression.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 public enum OperatorType
 {
@@ -46,7 +47,7 @@ public static class MathHelper
             List<uint> multipliedList = new List<uint>();
             for(uint i = 1; i <= primeFactorToNumFactorTuple.Value; i++)
             {
-                uint multiplier = (uint)System.Math.Pow(primeFactorToNumFactorTuple.Key, i);
+                uint multiplier = (uint)Math.Pow(primeFactorToNumFactorTuple.Key, i);
                 foreach (uint factor in allFactorList)
                     multipliedList.Add(multiplier * factor);
             }
@@ -59,7 +60,7 @@ public static class MathHelper
     public static uint[] GenerateUintRange( uint min, uint max, uint step=1 )
     {
         if( min>max )
-            throw new System.ArgumentException("Minimum bound must not be greater than maximum bound.");
+            throw new ArgumentException("Minimum bound must not be greater than maximum bound.");
         uint rangeLength = (max-min+step)/step;
         uint[] range = new uint[ rangeLength ];
         for(uint i=0;i<rangeLength;i++)
@@ -67,6 +68,46 @@ public static class MathHelper
             range[i] = min + i*step;
         }
         return range;
+    }
+
+    public static OperatorType GetInverseOperatorType( OperatorType operatorType )
+    {
+        switch(operatorType){
+            case OperatorType.PLUS:
+                return OperatorType.MINUS;
+            case OperatorType.MINUS:
+                return OperatorType.PLUS;
+            case OperatorType.MULTIPLY:
+                return OperatorType.DIVIDE;
+            case OperatorType.DIVIDE:
+                return OperatorType.MULTIPLY;
+            default:
+                throw new InvalidOperationException( string.Format("Invalid operator type {0}", operatorType ) );
+        }
+    }
+
+    public static uint ApplyOperator( uint firstOperand, uint secondOperand, OperatorType operatorType )
+    {
+        switch( operatorType )
+        {
+            case OperatorType.PLUS:
+                return firstOperand + secondOperand;
+            case OperatorType.MINUS:
+                if (secondOperand > firstOperand)
+                    throw new ArithmeticException( string.Format( "{0} - {1} will produce negative result.", firstOperand, secondOperand ) );
+                return firstOperand - secondOperand;
+            case OperatorType.MULTIPLY:
+                return firstOperand * secondOperand;
+            case OperatorType.DIVIDE:
+                if (secondOperand == 0)
+                    throw new DivideByZeroException( "The denominator cannot be zero." );
+                else if (firstOperand % secondOperand != 0)
+                    throw new ArithmeticException( string.Format( "The {0} is not divisible by {1}.", firstOperand, secondOperand ) );
+                else
+                    return firstOperand / secondOperand;
+            default:
+                throw new InvalidOperationException( string.Format("Invalid operator type {0}", operatorType ) );
+        }
     }
 }
 
@@ -88,6 +129,11 @@ public class ValueExpression: MathExpression
     {
         return value;
     }
+
+    public override string ToString()
+    {
+        return value.ToString();
+    }
 }
 
 public class OperationExpression: MathExpression
@@ -108,89 +154,132 @@ public class OperationExpression: MathExpression
         uint firstOperandValue = firstOperand.GetValue();
         uint secondOperandValue = secondOperand.GetValue();
 
+        return MathHelper.ApplyOperator( firstOperandValue, secondOperandValue, operatorType );
+    }
+
+    public override string ToString()
+    {
+        string firstOperandStr = firstOperand.ToString();
+        string secondOperandStr = secondOperand.ToString();
+
         switch( operatorType )
         {
             case OperatorType.PLUS:
-                return firstOperandValue + secondOperandValue;
+                return string.Format( "({0} + {1})", firstOperandStr, secondOperandStr );
             case OperatorType.MINUS:
-                if (secondOperandValue > firstOperandValue)
-                    throw new System.ArithmeticException();
-                return firstOperandValue - secondOperandValue;
+                return string.Format( "({0} - {1})", firstOperandStr, secondOperandStr );
             case OperatorType.MULTIPLY:
-                return firstOperandValue * secondOperandValue;
+                return string.Format( "({0} * {1})", firstOperandStr, secondOperandStr );
             case OperatorType.DIVIDE:
-                if (secondOperandValue == 0)
-                    throw new System.DivideByZeroException();
-                else if (firstOperandValue % secondOperandValue != 0)
-                    throw new System.ArithmeticException();
-                else
-                    return firstOperandValue / secondOperandValue;
+                return string.Format( "({0} / {1})", firstOperandStr, secondOperandStr );
             default:
-                throw new System.InvalidOperationException();
+                throw new InvalidOperationException( string.Format("Invalid operator type {0}", operatorType ) );
         }
     }
 }
 
 public static class MathExpressionGenerator
 {
-    /*
-    public static MathExpression GenerateMathExpressionByValue(int value, int numOperator, params OperatorType[] allowOperatorTypeArray)
+    public static MathExpression GenerateMathExpressionByValue(uint value, uint numOperator, uint expectedNumDigit, params OperatorType[] allowOperatorTypeArray)
     {
-        for(int i = 0; i < numOperator; i++)
-        {
+        if(allowOperatorTypeArray == null)
+            allowOperatorTypeArray = new OperatorType[]{ OperatorType.PLUS, OperatorType.MINUS, OperatorType.MULTIPLY, OperatorType.DIVIDE };
 
+        if(numOperator == 0)
+        {
+            return new ValueExpression( value );
         }
-    }*/
+
+        Random random = new Random();
+        OperatorType operatorType = allowOperatorTypeArray[ random.Next(0, allowOperatorTypeArray.Length) ];
+        uint biasNumDigit = 0;
+        uint[] lowerExpectedRange;
+        while(true)
+        {
+            try
+            {
+                lowerExpectedRange = GetLowerExpectedRange( value, operatorType, expectedNumDigit, biasNumDigit );
+                break;
+            }
+            catch( ArgumentException e )
+            {
+                biasNumDigit++;
+            }
+            catch( Exception e )
+            {
+                throw e;
+            }
+        }
+
+        uint secondOperandValue = lowerExpectedRange[ random.Next(0, lowerExpectedRange.Length) ];
+        uint firstOperandValue = MathHelper.ApplyOperator( value, secondOperandValue, MathHelper.GetInverseOperatorType( operatorType ) );
+
+        if(numOperator == 1)
+        {
+            ValueExpression firstOperand = new ValueExpression( firstOperandValue );
+            ValueExpression secondOperand = new ValueExpression( secondOperandValue );
+            return new OperationExpression( operatorType, firstOperand, secondOperand );
+        }
+        else
+        {
+            uint firstNumOperator = (uint)random.Next(0, (int)numOperator);
+            uint secondNumOperator = numOperator - firstNumOperator - 1;
+            MathExpression firstOperand = GenerateMathExpressionByValue( firstOperandValue, firstNumOperator, expectedNumDigit, allowOperatorTypeArray );
+            MathExpression secondOperand = GenerateMathExpressionByValue( secondOperandValue, secondNumOperator, expectedNumDigit, allowOperatorTypeArray );
+            return new OperationExpression( operatorType, firstOperand, secondOperand );
+        }
+    }
 
     public static uint[] GetLowerExpectedRange(uint result, OperatorType operatorType, uint expectedNumDigit, uint biasNumDigit=0)
     {
-        //  Initialize lower expected range. This range is used for:
-        //      1) Pick a randomized second operand (the lower one)
-        //      2) Compute a paried higher operand by doing the inverse operation with the result
         uint resultNumDigit = (uint)(result.ToString().Length);
         if(resultNumDigit < 1)
-            throw new System.ArithmeticException( "The number of result digits cannot be less than 1." );
+            throw new ArithmeticException( "The number of result digits cannot be less than 1." );
         switch (operatorType)
         {
             case OperatorType.DIVIDE:
                 if (biasNumDigit < resultNumDigit - 1)
-                    throw new System.ArithmeticException( "Too few number of bias digits." );
+                    throw new ArgumentException( "Too few number of bias digits." );
                 else if (resultNumDigit > expectedNumDigit)
-                    throw new System.ArithmeticException( "The number of expected digits cannot be less than of result." );
+                    throw new ArithmeticException( "The number of expected digits cannot be less than of result." );
                 else
                 {
-                    uint minExpectedNum = (uint)System.Math.Pow( 10, System.Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1);
-                    uint maxExpectedNum = (uint)((System.Math.Pow(10, expectedNumDigit) - 1) / result);
+                    uint minExpectedNum = (uint)Math.Pow( 10, Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1);
+                    uint maxExpectedNum = (uint)((Math.Pow(10, expectedNumDigit) - 1) / result);
                     return MathHelper.GenerateUintRange( minExpectedNum, maxExpectedNum );
                 }
             case OperatorType.MULTIPLY:
                 if (biasNumDigit < resultNumDigit/2)
-                    throw new System.ArithmeticException( "Too few number of bias digits." );
+                    throw new ArgumentException( "Too few number of bias digits." );
                 else
                 {
                     List<uint> primeFactorList = MathHelper.Factorize( result );
                     List<uint> allFactorList = MathHelper.GenerateAllFactorList( primeFactorList );
-                    allFactorList = allFactorList.FindAll( x => x >= System.Math.Pow( 10, System.Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1)
-                                                                && x <= System.Math.Pow( 10, biasNumDigit + 1 ) );
+                    allFactorList = allFactorList.FindAll( x => x >= Math.Pow( 10, Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1)
+                                                                && x <= Math.Pow( 10, biasNumDigit + 1 ) );
                     return allFactorList.ToArray();
                 }
             case OperatorType.MINUS:
                 {
-                    uint minExpectedNum = (uint)System.Math.Pow( 10, System.Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1);
-                    uint maxExpectedNum = (uint)((System.Math.Pow(10, expectedNumDigit) - 1) - result);
+                    uint minExpectedNum = (uint)Math.Pow( 10, Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1);
+                    uint maxExpectedNum = (uint)((Math.Pow(10, expectedNumDigit) - 1) - result);
                     return MathHelper.GenerateUintRange( minExpectedNum, maxExpectedNum );
                 }
             case OperatorType.PLUS:
                 if (expectedNumDigit > resultNumDigit)
-                    throw new System.ArithmeticException( "The number of expected digits cannot be greater than of result." );
-                else if( 2*(System.Math.Pow( 10, expectedNumDigit ) - 1) < result )
-                    throw new System.ArithmeticException( "Given number of expected digits will produce an empty range." );
+                    throw new ArithmeticException( "The number of expected digits cannot be greater than of result." );
+                else if( 2*(Math.Pow( 10, expectedNumDigit ) - 1) < result )
+                    throw new ArithmeticException( "Given number of expected digits will produce an empty range." );
+                else if( 2*(Math.Pow( 10, Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1 )) > result )
+                    throw new ArgumentException( "Given number of expected digits and bias digits will produce an empty range." );
+                else
                 {
-                    //uint minExpectedNum = (uint)System.Math.Pow( 10, System.Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1);
-                    //uint maxExpectedNum = System. (uint)((System.Math.Pow(10, expectedNumDigit) - 1) - result);
+                    uint minExpectedNum = (uint)Math.Max( result - Math.Pow( 10, expectedNumDigit ) - 1, Math.Pow( 10, Math.Max( expectedNumDigit - biasNumDigit, 1 ) - 1));
+                    uint maxExpectedNum = (uint)Math.Min( result - minExpectedNum, Math.Pow( 10, expectedNumDigit ) - 1 );
+                    return MathHelper.GenerateUintRange( minExpectedNum, maxExpectedNum );
                 }
             default:
-                throw new System.InvalidOperationException();
+                throw new InvalidOperationException();
         }
     }
 }
